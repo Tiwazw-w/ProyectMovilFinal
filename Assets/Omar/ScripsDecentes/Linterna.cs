168710f43bb97c3b74e045d82d7c7ee494231ce5
Tiwazw-w <140440132+Tiwazw-w@users.noreply.github.com>
2024-06-25 03:02:20 -0500

    Linterna_Mejorada

--- Assets/Omar/ScripsDecentes/Linterna.cs
+++ Assets/Omar/ScripsDecentes/Linterna.cs
@@ -5,24 +5,52 @@ using UnityEngine.UI;
 
 public class Linterna : MonoBehaviour
 {
-    public Light flashlight;  
+    public Light flashlight;
     public Button flashlightButton;
-
     private bool isFlashlightOn = false;
+
+    [Header("Energia")]
+
+    public float energia_Actual = 100f;
+    public float energia_Maxima = 100f;
+    public float ConsumodeEnergia = 0.5f;
+
+    [Header("Interfaz")]
+
+    public Image BarradeVida;
 
     void Start()
     {
 
         flashlight.enabled = false;
-
-
         flashlightButton.onClick.AddListener(ToggleFlashlight);
+        
+    }
+    private void Update()
+    {
+        NivelEnergia();
     }
 
     void ToggleFlashlight()
     {
         isFlashlightOn = !isFlashlightOn;
         flashlight.enabled = isFlashlightOn;
+    }
+
+    void NivelEnergia()
+    {
+        if(flashlight.enabled == true)
+        {
+            energia_Actual -= Time.deltaTime * ConsumodeEnergia;
+
+            if(energia_Actual <= 0)
+            {
+                energia_Actual = 0;
+                flashlight.enabled = false;
+            }
+        }
+
+        BarradeVida.fillAmount = energia_Actual / energia_Maxima;
     }
 }
 
